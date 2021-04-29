17de50c0acba8eae721aaa27c2f07d13f5d12b55
Gary Napier <napierg@Garys-MacBook-Pro.local>
2021-04-30 00:01:44 +0100

    test

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 
 AUTHOR = 'Gary Napier'
 SITENAME = 'CCIE Journal'
-SITEURL = ''
+SITEURL = 'cciejournal.com'
 
 PATH = 'content'
 
@@ -19,10 +19,9 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('CISCO', 'https://cisco.com/'),
+         ('CCIE', 'https://cisco.com'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
